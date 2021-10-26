986664e58c7f4852dece63c335608942b913db25
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-26 10:52:55 +0500

    vegvre

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -536,7 +536,7 @@ export default class Dashboard extends Component {
                   iconLayout: "default#image",
                 }}
                 properties={{
-                  hintContent: `<h6><b className="personStyle">33 - maktab</b></h6>`,
+                  hintContent: `<h6><b className="personStyle">1 - maktab</b></h6>`,
                 }}
                 modules={["geoObject.addon.hint"]}
               />
